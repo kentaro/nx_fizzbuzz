fcdd0b043c8471b61b98cd2d652cba2d026729c4
Kentaro Kuribayashi <kentarok@gmail.com>
2021-02-26 01:42:45 +0900

    init with `mix new nx_fizzbuzz`

---
+++ lib/nx_fizzbuzz.ex
@@ -0,0 +1,18 @@
+defmodule NxFizzbuzz do
+  @moduledoc """
+  Documentation for `NxFizzbuzz`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NxFizzbuzz.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nx_fizzbuzz_test.exs
@@ -0,0 +1,8 @@
+defmodule NxFizzbuzzTest do
+  use ExUnit.Case
+  doctest NxFizzbuzz
+
+  test "greets the world" do
+    assert NxFizzbuzz.hello() == :world
+  end
+end
