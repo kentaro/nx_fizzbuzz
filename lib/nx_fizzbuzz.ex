b91980ddf8b2fe074f7101a0cd8dce8a27d6d8c6
Kentaro Kuribayashi <kentarok@gmail.com>
2021-02-26 02:47:25 +0900

    add dataset generator

--- lib/nx_fizzbuzz.ex
+++ lib/nx_fizzbuzz.ex
@@ -1,18 +1,2 @@
-defmodule NxFizzbuzz do
-  @moduledoc """
-  Documentation for `NxFizzbuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NxFizzbuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+defmodule NxFizzBuzz do
 end
---
+++ lib/nx_fizzbuzz/dataset.ex
@@ -0,0 +1,31 @@
+defmodule NxFizzBuzz.Dataset do
+  def generate_dataset(size) do
+    max = :math.pow(2, 32) |> round() |> Kernel.-(1)
+
+    randn =
+      1..size
+      |> Enum.map(fn _ -> Enum.random(0..max) end)
+
+    labels =
+      randn
+      |> Enum.map(&fizz_buzz/1)
+
+    {
+      Nx.tensor(randn, type: {:u, 32}),
+      Nx.tensor(labels, type: {:u, 32})
+    }
+  end
+
+  defp fizz_buzz(n) do
+    cond do
+      # FizzBuzz
+      rem(n, 15) == 0 -> [0, 0, 0, 1]
+      # Buzz
+      rem(n, 5) == 0 -> [0, 0, 1, 0]
+      # Fizz
+      rem(n, 3) == 0 -> [0, 1, 0, 0]
+      # number
+      true -> [n, 0, 0, 0]
+    end
+  end
+end
